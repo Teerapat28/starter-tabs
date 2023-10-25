1e9917e43e259c57b1e2f42b7f0491495ea5d7c1
유애나 <77397206+Teerapat28@users.noreply.github.com>
2023-10-25 17:41:02 +0700

    Tabs Project Fetch Data UUID Library

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
+
+const url = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+      {/* {buttons} */}
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      {/* {job info} */}
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
+};
+export default App;
